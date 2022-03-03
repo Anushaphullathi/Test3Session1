294a717c70c788afaf0b644e0211ddc5fbce3180
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-03-03 10:18:48 +0000

    ..

--- p4original.c
+++ p4original.c
@@ -1,19 +1,19 @@
 #include<stdio.h>
 int input_degree()
 {
-  
+  int n;
+  printf("Enter the degree of polynomial\n");
+  scanf("%d",&n);
 }
 float input_x()
-{
-  
-}
-float input_x()
-{
-  
+{ 
+  int x;
+  printf("Enter the value of x");
+  scanf("%d",&x);
 }
 void input_coefficients(int n, float a[n])
 {
-  
+  printf("Enter the cofficients of polynomial")
 }
 float evaluate_polynomial(int n, float a[n], float x)
 {

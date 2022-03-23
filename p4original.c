f15160245ae452b9f7c45eebde3a0d80b549ddd8
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-03-23 18:31:50 +0000

    p5 23/3/22

--- p4original.c
+++ p4original.c
@@ -9,13 +9,13 @@ int input_degree()
 int input_x()
 { 
   int x;
-  printf("Enter the value of x");
+  printf("Enter the value of x\n");
   scanf("%d",&x);
   return x;
 }
 void input_coefficients(int n, int a[n])
 { 
-  printf("Enter the cofficients of polynomial");
+  printf("Enter the cofficients of polynomial\n");
   for(int i=0;i<n;i++)
     {
      scanf("%d",&a[i]);
@@ -32,7 +32,10 @@ int evaluate_polynomial(int n, int a[n], int x)
 }
 void out_put(int n, int a[n], int x, int result)
 {
-  printf("%d",result);
+  int i;
+ for(int i=1;i<n-1;i++)
+    printf("%0.2f*x^%d+",a[i],i);
+  printf("%0.2f*x^%d is %f\n",a[i],i,result);
 }
 int main()
 {
@@ -42,6 +45,6 @@ int main()
   x=input_x();
   input_coefficients(n,a);
   c=evaluate_polynomial(n,a,x);
-  out_put(n,a,x,c);
+  out_put(n,a[n],x,c);
   return 0;
 }
--- p5original.c
+++ p5original.c
@@ -1,1 +1,43 @@
-#include<stdio.h
+#include<stdio.h>
+int input_size()
+{
+  int n;
+  printf("Enter the size of array\n");
+  scanf("%d",&n);
+  return n;
+}
+void input_array(int n, int a[n])
+{
+  printf("Enter the array elements\n");
+  for(int i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+}
+int find_largest(int n, int a[n])
+{
+   int max=a[0],k=0;
+  for(int i=1;i<n;i++)
+    {
+      if(max<a[i]){
+        max=a[i];
+        k=i;
+        }
+    }
+  return k;
+}
+void out_put(int n, int a[n], int largest)
+{
+  printf("the index of largest element %d in array is %d\n",a[largest],largest);
+}
+ int main()
+{
+  int n=input_size();
+  int a[n];
+  input_array(n,a);
+  int max=find_largest(n,a);
+  out_put(n,a,max);
+  return 0;
+}
+
+ 

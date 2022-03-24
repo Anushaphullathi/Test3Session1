d80e5c6d1c6cdee2f7a1c9ef0c9949b503b8ee9f
Anushaphullathi <96220359+Anushaphullathi@users.noreply.github.com>
2022-03-24 16:44:22 +0000

    p1 24/3/22

---
+++ p6original.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char *a)
+{
+  
+}
+int count_words(char *string)
+{
+  
+}
+void output(char *string, int no_words)
+{
+  
+}
+int main()
+{
+  return 0;
+}
